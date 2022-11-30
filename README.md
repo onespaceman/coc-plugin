234c379f4d5715cd96268d2c8f4a337378f4e771
onespaceman <onespaceman@users.noreply.github.com>
2022-11-30 14:23:27 -0500

    Initial plugin

--- README.md
+++ README.md
@@ -1,73 +1,53 @@
-# Obsidian Sample Plugin
+# TTRPG Helper Tools
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Some personal tools for ttrpgs (mainly Call of Cthulhu)
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+![screenshot](scrot.png)
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+## Features
+- Calculate and insert 1/2 and 1/5 stats for Call of Cthulhu
+- Mouseover glossary
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## Usage
+### Stat Calculator
+- Start scraping stats by calling it from the frontmatter: `statcalc: true`
+- Place any stats you want calculated between curly braces: `{100}` will result in `100 | 50 | 20`
+- Style the new elements with `.ttrpgtools-inline` and `.ttrpgtools-hover`
+- Options can be added as a block list or inline
 
-## First time developing plugins?
+`statmod : [style: inline, separator: "-"]`
+```
+statmod :
+ - style: inline
+ - separator: "-"
+ ```
 
-Quick starting guide for new plugin devs:
+| Option    | Description                                        | Default |
+| --------- | -------------------------------------------------- | ------- |
+| position  | location of the tooltip (left, right, top, bottom) | top     |
+| separator | The separator between stats                        | " \| "  |
+| style     | `inline` or `toolip`                               | inline  |
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+### Glossary
+- Call it from the frontmatter: `glossary: filename`
+- The glossary must be a MD file with the text in JSON
+- Aliases may be added by setting the value to `glossary.term`
+- Style the new elements using `.ttrpgtools-glossary`
 
-## Releasing new releases
+Example:
+```
+{
+"BLD":"glossary.Build",
+"Build":"SIZ + STR\n2-64: -2\n65-84: -1\n85-124: 0\n125-164: 1\n+1 for each additional 80pts",
+"DB":"glossary.Damage Bonus",
+"Damage Bonus":"SIZ + STR\n2-64: -2\n65-84: -1\n85-124: 0\n125-164: +1D4\n165-204: +1D6\n+1D6 for each additional 80pts",
+"HP":"=CON+SIZ/10",
+"Max Sanity":"=99-Cthulhu Mythos"
+}
+```
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+## Disclaimer
+This is poorly coded. I made this for personal use, so it probably interferes with/breaks other plugins. Use at your own risk
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+## Bonus
+- CSS style based on Homebrewery/CritterDB. Enable with `cssClass: statblock`
--- main.ts
+++ main.ts
@@ -1,137 +1,93 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownPostProcessorContext} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TTRPGtoolsPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log('loading TTRPG Helper Tools')
+		this.registerMarkdownPostProcessor(this.statCalc.bind(this))
+		this.registerMarkdownPostProcessor(this.glossary.bind(this))
 	}
 
 	onunload() {
-
+		console.log('unloading TTRPG Helper Tools')
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async statCalc(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+		interface options {
+			separator?: string
+			style?: "inline" | "tooltip" 
+			position?: "top" | "bottom" | "right" | "left"
+		}
+		if (typeof(ctx.frontmatter) == "undefined") return
+		if (typeof(ctx.frontmatter.statcalc) == "undefined") return
+
+		const frontmatterPre = ctx.frontmatter.statcalc
+		const frontmatter: options = {}
+		for (let i = 0; i < frontmatterPre.length; i++) {
+			Object.assign(frontmatter, frontmatterPre[i])
+		} 
+		const separator = frontmatter.separator || " | "
+		const tooltipPostion = frontmatter.position || "top"
+		const style = frontmatter.style || "inline"
+		
+		const nodeList = el.querySelectorAll("p, h1, h2, h3, h4, h5, h6, th, td, li, ul, ol")
+		if (!nodeList.length) return
+		nodeList.forEach(node => {
+			const text = node.innerHTML
+
+			if (text?.match(/{[0-9]*}/g)) {
+				node.innerHTML = text.replace(/{[0-9]*}/g, function (x) {
+					const statInt = parseInt(x.replace(/\{|\}/g, ''))
+					if (style == "inline") {
+						return `${statInt}<span class="ttrpgtools-inline">${separator}${Math.floor(statInt / 2)}${separator}${Math.floor(statInt / 5)}</span>`
+					} else if (style == "tooltip") {
+						return `<span class="ttrpgtools-hover" aria-label="${Math.floor(statInt / 2)}${separator}${Math.floor(statInt / 5)}" aria-label-position="${tooltipPostion}">${statInt}</span>`
+					} else return `${statInt}`
+				})
+			}
+
+		})
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	async glossary(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+		interface glos {
+			[index: string]: string;
+		}
+		if (typeof(ctx.frontmatter) == "undefined") return
+		if (typeof(ctx.frontmatter.glossary) == "undefined") return
+		
+		const frontmatterPre = ctx.frontmatter.glossary
+		const gloss = frontmatterPre
+		
+		const files = this.app.vault.getMarkdownFiles()
+		const glossFile = files.find(file => {
+			if (file.basename == gloss) {
+				return file
+			}
+		})
+		if (!glossFile) return
+		const glossaryText = await this.app.vault.cachedRead(glossFile)
+		const glossary: glos = JSON.parse(glossaryText)
+		for (const [key, value] of Object.entries(glossary)) {
+			if (value.match("glossary.")) {
+				const pp = value.replace("glossary.", "")
+				if (glossary[pp]) {
+					glossary[key] = glossary[pp]
+				}				
+			}
+		}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const nodeList = el.querySelectorAll("p, h1, h2, h3, h4, h5, h6, th, td, li, ul, ol")
+		if (!nodeList.length) return
+		nodeList.forEach(node => {
+			const text = node.innerHTML
+			for (const [key, value] of Object.entries(glossary)) {
+				const regex = new RegExp(`\\b${key}\\b`, "gm")
+				if (text?.match(regex)) {
+					node.innerHTML = text.replace(regex, `<span class="ttrpgtools-glossary" aria-label="${value}" aria-label-position="top">${key}</span>`)
+				}
+			}
+		})
+		
 	}
 }
